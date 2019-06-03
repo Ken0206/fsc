a26a5399543d4a46292dae9396d0e0ba1e75f325
Ken Peng <cryptdoc@gmail.com>
2019-06-03 11:39:03 +0800

    修正絕對路徑中間有空白字元問題，更新選單

--- nouser_files.sh
+++ nouser_files.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Description: nouser files
-# date: 2019-04-09
+# date: 2019-06-03
 
 if [ ! -f "/src/chkau/export_env" ] ; then
   echo ''
@@ -29,6 +29,9 @@ else
   OS="AIX"
 fi
 
+direct_option="direct"
+option_1="$1"
+
 
 find_files() {
   timeStamp=$(date +%Y%m%d_%H%M%S)
@@ -43,20 +46,15 @@ find_files() {
   find / -nouser -type f 2>/dev/null | sort > ${newFind}
   newFindCount=$(cat ${newFind} | wc -l)
   
-  #echo "   日期      時間      權限     UID   GID     完整路徑" >> ${output_newFind}
-  #echo "-------------------------------------------------------------------" >> ${output_newFind}
-  for i in $(cat ${newFind}) ; do
-    if [ -f "${i}" ] || [ -d "${i}" ] ; then
-      check_ftime ${i}
-      ls_file=$(ls -ld ${i} | awk '{print $1" "$3" "$4" "$9}')
-      echo ${ftime}" "${ls_file}  >> ${base_newFind}
-    fi
-  done
+  #for i in $(cat ${newFind}) ; do
+  while read file_name ; do
+    check_ftime "${file_name}"
+    ls_file=$(ls -ld "${file_name}" | awk '{print $1" "$3" "$4}')
+    echo "${ftime} ${ls_file} ${file_name}"  >> ${base_newFind}
+  done < ${newFind}
   cat ${base_newFind} >> ${output_newFind}
-  #echo "-------------------------------------------------------------------" >> ${output_newFind}
-  #echo "   日期      時間      權限     UID   GID     完整路徑" >> ${output_newFind}
-  echo '' >> ${output_newFind}
-  echo "  Total  : ${newFindCount}" >> ${output_newFind}
+  #echo '' >> ${output_newFind}
+  #echo "  Total  : ${newFindCount}" >> ${output_newFind}
 
   report=${0%/*}/${RANDOM}.report.temp
   if [ ! -f "${base_line}" ] ; then
@@ -65,10 +63,10 @@ find_files() {
     echo "查無基準檔，建立基準檔" >> ${report}
   else
     base_line_d=${0%/*}/${RANDOM}.base_line_d.temp
-    awk '{print $6}' ${base_line} | sort > ${base_line_d}
+    awk -F' /' '{print "/"$2}' ${base_line} | sort > ${base_line_d}
     t1=${0%/*}/${RANDOM}.t1.temp
     t2=${0%/*}/${RANDOM}.t2.temp
-    diff ${base_line_d} ${newFind} | grep "^<" | awk '{print $2}' >> ${t1}
+    diff ${base_line_d} ${newFind} | grep "^<" | sed 's/^< //' >> ${t1}
     delCount=$(cat ${t1} | wc -l)
     if [ -s "${t1}" ] 
     then 
@@ -77,7 +75,7 @@ find_files() {
       echo "減少清單 :" >> ${report}
       cat ${t1} >> ${report}
     fi
-    diff ${base_line_d} ${newFind} | grep "^>" | awk '{print $2}' >> ${t2}
+    diff ${base_line_d} ${newFind} | grep "^>" | sed 's/^> //' >> ${t2}
     addCount=$(cat ${t2} | wc -l)
     if [ -s "${t2}" ]
     then
@@ -85,13 +83,14 @@ find_files() {
       echo "比基準檔 新增數量 : ${addCount}" >> ${report}
       echo "新增清單 :" >> ${report}
       echo "   日期      時間      權限     UID   GID     完整路徑" >> ${report}
-      echo "-------------------------------------------------------------------" >> ${report}
-      for i in $(cat ${t2}) ; do
-        check_ftime ${i}
-        ls_file=$(ls -ld ${i} | awk '{print $1" "$3" "$4" "$9}')
-        echo ${ftime}" "${ls_file}  >> ${report}
-      done
-      echo "-------------------------------------------------------------------" >> ${report}
+      echo "------------------------------------------------------------------------------------------" >> ${report}
+      #for i in $(cat ${t2}) ; do
+      while read file_name ; do
+        check_ftime "${file_name}"
+        ls_file=$(ls -ld "${file_name}" | awk '{print $1" "$3" "$4}')
+        echo "${ftime} ${ls_file} ${file_name}" >> ${report}
+      done < ${t2}
+      echo "------------------------------------------------------------------------------------------" >> ${report}
       echo "   日期      時間      權限     UID   GID     完整路徑" >> ${report}
     fi
     rm -f ${t1} ${t2}
@@ -105,13 +104,13 @@ find_files() {
   #cat ${base_line_d} > ${base_line}
   fi
 
-  check_ftime ${base_line}
+  check_ftime "${base_line}"
   echo '' >> ${report}
   echo "基 準 檔 : ${base_line}" >> ${report}
   echo "檔案時間 : ${ftime}" >> ${report}
   echo "  Total  : $(cat ${base_line} | wc -l)" >> ${report}
 
-  check_ftime ${output_newFind}
+  check_ftime "${output_newFind}"
   echo '' >> ${report}
   echo "目前搜尋結果檔案清單 : "${output_newFind} >> ${report}
   echo "檔案時間 : ${ftime}" >> ${report}
@@ -123,17 +122,17 @@ find_files() {
 
   cat ${report} >> ${log}
   rm -f ${t1} ${t2} ${newFind} ${report} ${base_line_d} ${base_newFind}
+  clear
   cat ${log}
 }
 
 
 check_ftime() {
   if [ "${OS}" == "Linux" ] ; then
-    ftime=$(ls -ld $1 --time-style=full-iso | awk '{print $6" "$7}' | awk -F'.' '{print $1}')
+    ftime=$(ls -ld "$1" --time-style=full-iso | awk '{print $6" "$7}' | awk -F'.' '{print $1}')
   else
-    ftime=$(istat $1 | grep "modified" | awk '{print $8"-"$4"-"$5" "$6}')
+    ftime=$(istat "$1" | grep "modified" | awk '{print $8"-"$4"-"$5" "$6}')
   fi
-
 }
 
 
@@ -143,6 +142,7 @@ view_log() {
     echo '沒有搜尋紀錄!'
     return
   fi
+  clear
   cat ${log}
 }
 
@@ -155,14 +155,16 @@ view_base_line() {
   else
     clear
     if [ -s "${base_line}" ] ; then
+      echo ''
       echo "   權限     UID   GID   完整路徑"
-      echo "--------------------------------------------------"
-      for i in $(cat ${base_line}) ; do
-        if [ -f "${i}" ] || [ -d "${i}" ] ; then
-          ls -ld ${i} | awk '{print $1" "$3" "$4" "$9}'
-        fi
-      done
-      echo "--------------------------------------------------"
+      echo "------------------------------------------------------------------------------------------"
+      #for i in $(cat ${base_line}) ; do
+      #while read file_name ; do
+      #  file_data_p1=$(ls -ld "${file_name}" | awk '{print $1" "$3" "$4}')
+      #  echo "${file_data_p1} ${file_name}"
+      #done < "${base_line}"
+      cat ${base_line}
+      echo "------------------------------------------------------------------------------------------"
       echo "   權限     UID   GID   完整路徑"
     else
       echo ''
@@ -205,6 +207,7 @@ main() {
     read choice
     case $choice in
     1)
+      echo '搜尋中 ...'
       find_files
       echo ''
       echo "請按Enter鍵繼續"
@@ -236,7 +239,13 @@ main() {
 }
 
 
-find_files
+
+if [ "${option_1}" != "${direct_option}" ] ; then
+  main
+else
+  find_files
+fi
+
 # 刪除60天以上的舊報告
 find /src/chkau/report -mtime +60 -type f -exec rm -f {} \;
 exit 0
